2ea7cb96a1bb04c771a1ad90dce4e22e029ae438
AlleriaWindrunner <eleven.hm@vip.163.com>
2023-01-15 21:19:51 +0800

    feature:release 0.4.25 version.

---
+++ src/test/java/org/blockchain/solc/compiler/test/SolcTest.java
@@ -0,0 +1,24 @@
+package org.blockchain.solc.compiler.test;
+
+import java.io.IOException;
+
+import org.blockchain.solc.compiler.SolidityCompiler;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolcTest {
+
+    @Test
+    public void smSolcTest() throws IOException {
+        String versionOutput = SolidityCompiler.runGetVersionOutput(true);
+        Assert.assertTrue(versionOutput.contains("0.4.25"));
+        Assert.assertTrue(versionOutput.toLowerCase().contains("gm"));
+    }
+
+    @Test
+    public void ecdsaSolcTest() throws IOException {
+        String versionOutput = SolidityCompiler.runGetVersionOutput(false);
+        Assert.assertTrue(versionOutput.contains("0.4.25"));
+        Assert.assertTrue(!versionOutput.toLowerCase().contains("gm"));
+    }
+}

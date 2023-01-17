b1d8b2736b19ce394b0058c055ee13dc000e1f08
AlleriaWindrunner <eleven.hm@vip.163.com>
2023-01-17 18:22:45 +0800

    fix:add gradle-wrapper.jar for action CI.

--- src/main/java/io/github/alleriawindrunner/solc/compiler/Solc.java
+++ src/main/java/io/github/alleriawindrunner/solc/compiler/Solc.java
@@ -1,4 +1,4 @@
-package org.blockchain.solc.compiler;
+package io.github.alleriawindrunner.solc.compiler;
 
 import java.io.File;
 import java.io.IOException;
--- src/test/java/io/github/alleriawindrunner/solc/compiler/test/SolcTest.java
+++ src/test/java/io/github/alleriawindrunner/solc/compiler/test/SolcTest.java
@@ -1,8 +1,8 @@
-package org.blockchain.solc.compiler.test;
+package io.github.alleriawindrunner.solc.compiler.test;
 
 import java.io.IOException;
 
-import org.blockchain.solc.compiler.SolidityCompiler;
+import io.github.alleriawindrunner.solc.compiler.SolidityCompiler;
 import org.junit.Assert;
 import org.junit.Test;
 
